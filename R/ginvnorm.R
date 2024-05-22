85b83141f5b4d3472323beac841a64c824634ce7
David Gerard <gerard.1787@gmail.com>
2024-05-22 15:46:05 -0400

    add hypergeo special

--- R/ginvnorm.R
+++ R/ginvnorm.R
@@ -32,3 +32,116 @@ hypergeo1f1 <- function(a, b, z, method = c("series", "integral"), nterms = 10) 
   }
   return(val)
 }
+
+#' log-sum-exp trick
+#'
+#' Calculates log(w_1e^{x_1} + w_2e^{x_2} + ... + w_ne^{x_n})
+#' for given values of x and w
+#'
+#' @param x The log of the summands
+#' @param weights The weights. Possibly negative, as long as sum is positive,
+#'     but I don't check this.
+#'
+#'
+#' @author David Gerard
+#'
+#' @noRd
+log_sum_exp <- function(x, weights = rep(1, length(x)), na.rm = FALSE) {
+  stopifnot(length(weights) == length(x))
+  if (na.rm) {
+    weights <- weights[!is.na(x)]
+    x <- x[!is.na(x)]
+  } else if (any(is.na(x))) {
+    return(NA)
+  }
+  if (all(x == -Inf)) {
+    return(-Inf)
+  } else if (any(x == Inf)) {
+    return(Inf)
+  } else {
+    z <- max(x)
+    return(log(sum(weights * exp(x-z))) + z)
+  }
+}
+
+
+#' Hypergeometric1F1(a/2, 1/2, z) for positive integer a and positive number z
+#'
+#' Uses recurrence relation of https://dlmf.nist.gov/13.3#i
+#' 13.3.1
+#'
+#' `Hypergeometric1F1[1/2, 1/2, z]` = `exp(z)`
+#' `Hypergeometric1F1[3/2, 1/2, z]` = `exp(z) * (1 + 2 * z)`
+#' `Hypergeometric1F1[-1, 1/2, z]` = `1 - 2 * z`
+#' `Hypergeometric1F1[0, 1/2, z]` = `1`
+#' `Hypergeometric1F1[1, 1/2, z]` = `1 + exp(z) * sqrt(pi) * sqrt(z) * (2 * pnorm(sqrt(2 * z)) - 1)`
+#'
+#' Recurrence relation:
+#' `(b - a) * M(a - 1, b, z) + (2 * a - b + z) * M(a, b, z) - a * M(a + 1, b, z) = 0`
+#'
+#'
+#' @param a an integer
+#' @param z a positive number
+#'
+#' @author David Gerard
+#'
+#' @noRd
+hg1f1_special <- function(a, z, log = TRUE) {
+  stopifnot(z > 0)
+  if (a %% 2  == 1) {
+    ## First two elements on log-scale
+    m1_2 <- z
+    m3_2 <- z + log(1 + 2 * z)
+
+    if (a == 1) {
+      sval <- m1_2
+    } else if (a == 3) {
+      sval <- m3_2
+    } else {
+      m_nm1 <- m1_2
+      m_n <- m3_2
+      n <- 1.5
+      maxit <- (a - 3) / 2
+      for (i in 1:maxit) {
+        m_np1 <- log_sum_exp(x = c(m_nm1, m_n), weights = c(0.5 - n, 2 * n - 0.5 + z)) - log(n)
+        n <- n + 1
+        m_nm1 <- m_n
+        m_n <- m_np1
+      }
+      sval <- m_np1
+    }
+  } else if (a %% 2 == 0) {
+    ## First two elements on log-scale
+    m0_2 <- 0
+    m2_2 <- log_sum_exp(c(0, z + 0.5 * log(pi) + 0.5 * log(z) + log((2 * stats::pnorm(sqrt(2 * z)) - 1))))
+    if (a == 0) {
+      sval <- m0_2
+    } else if (a == 2) {
+      sval_ <- m2_2
+    } else {
+      m_nm1 <- m0_2
+      m_n <- m2_2
+      n <- 1
+      maxit <- (a - 2) / 2
+      for (i in 1:maxit) {
+        m_np1 <- log_sum_exp(x = c(m_nm1, m_n), weights = c(0.5 - n, 2 * n - 0.5 + z)) - log(n)
+        n <- n + 1
+        m_nm1 <- m_n
+        m_n <- m_np1
+      }
+      sval <- m_np1
+    }
+  } else {
+    stop("a is not an integer")
+  }
+  if (!log) {
+    sval <- exp(sval)
+  }
+  return(sval)
+}
+
+
+
+
+
+
--- tests/testthat/test-ginvnorm.R
+++ tests/testthat/test-ginvnorm.R
@@ -10,4 +10,39 @@ test_that("1F1 works", {
   expect_equal(hypergeo1f1(a = 1, b = 2, z = 2, method = "series"), (exp(2) - 1) / 2, tolerance = 1e-3)
   expect_equal(hypergeo1f1(a = 3, b = 5, z = 3, method = "series"), (exp(3) - 4) * 4 / 9, tolerance = 1e-3)
   expect_equal(hypergeo1f1(a = 4, b = 3, z = 1, method = "series"), exp(1) * 4 / 3, tolerance = 1e-3)
+
+
+  expect_equal(
+    exp(hg1f1_special(3, 2)),
+    hypergeo1f1(a = 3/2, b = 1/2, z = 2, method = "series", nterms = 100)
+  )
+
+  expect_equal(
+    exp(hg1f1_special(4, 2)),
+    hypergeo1f1(a = 4/2, b = 1/2, z = 2, method = "series", nterms = 100)
+  )
+
+  expect_equal(
+    exp(hg1f1_special(11, 2)),
+    hypergeo1f1(a = 11/2, b = 1/2, z = 2, method = "series", nterms = 100)
+  )
+
+  expect_equal(
+    exp(hg1f1_special(12, 2)),
+    hypergeo1f1(a = 12/2, b = 1/2, z = 2, method = "series", nterms = 100)
+  )
+
+})
+
+test_that("lse works", {
+  x <- c(1, 3, -1)
+  expect_equal(
+    log(sum(exp(x))),
+    log_sum_exp(x)
+  )
+  expect_equal(log_sum_exp(c(-Inf, 1)), 1)
+  expect_equal(log_sum_exp(c(-Inf, -Inf)), -Inf)
+  expect_equal(log_sum_exp(c(1, Inf)), Inf)
+  expect_equal(log_sum_exp(c(1, 2, NA), na.rm = FALSE), NA)
+  expect_equal(log_sum_exp(c(1, 2, NA), na.rm = TRUE), log(sum(exp(c(1, 2)))))
 })
