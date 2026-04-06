4d8847798803052abbaac0a24a56b9a36d784226
dcgerard <gerard.1787@gmail.com>
2026-04-06 15:41:10 -0400

    add weight

--- R/n2.R
+++ R/n2.R
@@ -4,29 +4,34 @@
 #' @param n sample size
 #' @param eta2 multiplier squared
 #' @param nu (mu - A) / sigma
+#' @param wt Prior weight for A. This is interpreted as how many replicates of
+#'     A the t-statistic has.
 #'
 #' @author David Gerard
 #'
 #' @noRd
-w_z <- function(z, n, eta2, nu) {
+w_z <- function(z, n, eta2, nu, wt = 1) {
   ## ((n * z - sqrt(n) * nu)^2 - eta2 * (z + sqrt(n) * nu)^2) / ((n + 1) * eta2)
-  ((n^2 - eta2) * z^2 - 2 * (n^(1.5) + sqrt(n) * eta2) * nu * z + (1 - eta2) * n * nu^2) / ((n + 1) * eta2)
+  ## ((n^2 - eta2) * z^2 - 2 * (n^(1.5) + sqrt(n) * eta2) * nu * z + (1 - eta2) * n * nu^2) / ((n + 1) * eta2) ## old way
+  wt^2 * (n + wt - 1) * (n * z / wt - sqrt(n) * nu)^2 / ((n + wt) * n * eta2) - wt * (z + sqrt(n) * nu)^2 / (n + wt)
 }
 
 #' Integrand.
 #'
-#' This is the integral from 0 to w_z wrt chi-squared pdf times standard normal pdf
+#' This is the integral from 0 to w_z wrt chi-squared cdf times standard normal pdf
 #'
 #' @param z normal data
 #' @param n sample size
 #' @param eta2 squared multiplier
 #' @param nu (mu - A) / sigma
+#' @param wt Prior weight for A. This is interpreted as how many replicates of
+#'     A the t-statistic has.
 #'
 #' @author David Gerard
 #'
 #' @noRd
-fn <- function(z, n, eta2, nu) {
-  stats::pchisq(q = w_z(z = z, n = n, eta2 = eta2, nu = nu), df = n - 1) * stats::dnorm(x = z)
+fn <- function(z, n, eta2, nu, wt = 1) {
+  stats::pchisq(q = w_z(z = z, n = n, eta2 = eta2, nu = nu, wt = wt), df = n - 1) * stats::dnorm(x = z)
 }
 
 #' Alpha given eta2 and nu
@@ -36,12 +41,14 @@ fn <- function(z, n, eta2, nu) {
 #' @param n sample size
 #' @param eta2 squared multiplier
 #' @param nu (mu - A) / 2
+#' @param wt Prior weight for A. This is interpreted as how many replicates of
+#'     A the t-statistic has.
 #'
 #' @author David Gerard
 #'
 #' @noRd
-obj_fn <- function(n, eta2, nu) {
-  stats::integrate(f = fn, lower = -Inf, upper = Inf, n = n, eta2 = eta2, nu = nu)[[1]]
+obj_fn <- function(n, eta2, nu, wt = 1) {
+  stats::integrate(f = fn, lower = -Inf, upper = Inf, n = n, eta2 = eta2, nu = nu, wt = wt)[[1]]
 }
 
 #' Largest alpha given eta2
@@ -50,11 +57,13 @@ obj_fn <- function(n, eta2, nu) {
 #'
 #' @param n sample size
 #' @param eta2 squared multiplier
+#' @param wt Prior weight for A. This is interpreted as how many replicates of
+#'     A the t-statistic has.
 #'
 #' @author David Gerard
 #'
 #' @noRd
-worst_alpha <- function(n, eta2) {
+worst_alpha <- function(n, eta2, wt = 1) {
   oout <- stats::optim(
     par = 1,
     fn = obj_fn,
@@ -63,7 +72,8 @@ worst_alpha <- function(n, eta2) {
     upper = Inf,
     control = list(fnscale = -1),
     n = n,
-    eta2 = eta2)
+    eta2 = eta2,
+    wt = wt)
   c(alpha = oout$value, nu = oout$par)
 }
 
@@ -75,16 +85,18 @@ worst_alpha <- function(n, eta2) {
 #'
 #' @param alpha error probability
 #' @param n sample size
+#' @param wt Prior weight for A. This is interpreted as how many replicates of
+#'     A the t-statistic has.
 #'
 #' @author David Gerard
 #'
 #' @noRd
-find_bounds <- function(alpha, n) {
+find_bounds <- function(alpha, n, wt = 1) {
   low <- 0.01
   high <- exp(1)
   alpha_now <- 1
   while (alpha_now > alpha) {
-    aout <- worst_alpha(n = n, eta2 = high)
+    aout <- worst_alpha(n = n, eta2 = high, wt = wt)
     alpha_now <- aout[["alpha"]]
     if (alpha_now > alpha) {
       low <- high
@@ -102,10 +114,10 @@ find_bounds <- function(alpha, n) {
 #' @author David Gerard
 #'
 #' @noRd
-eta_alpha <- function(alpha, n) {
-  bounds <- find_bounds(alpha = alpha, n = n)
-  eout <- stats::uniroot(f = function(eta2) alpha - worst_alpha(n = n, eta2 = eta2)[["alpha"]], interval = bounds)
-  aout <- worst_alpha(n = n, eta2 = eout$root)
+eta_alpha <- function(alpha, n, wt = 1) {
+  bounds <- find_bounds(alpha = alpha, n = n, wt = wt)
+  eout <- stats::uniroot(f = function(eta2) alpha - worst_alpha(n = n, eta2 = eta2, wt = wt)[["alpha"]], interval = bounds)
+  aout <- worst_alpha(n = n, eta2 = eout$root, wt = wt)
   c(eta = sqrt(eout$root), nu = aout[["nu"]], alpha = aout[["alpha"]])
 }
 
@@ -121,6 +133,8 @@ eta_alpha <- function(alpha, n) {
 #' @param x The vector of data
 #' @param A The prior mean
 #' @param level The level of the interval
+#' @param wt Prior weight for A. This is interpreted as how many replicates of
+#'     A the t-statistic has.
 #'
 #' @author David Gerard
 #'
@@ -138,24 +152,72 @@ eta_alpha <- function(alpha, n) {
 #' aug_t(x, A = 100)
 #' stats::t.test(x)$conf.int
 #'
-aug_t <- function(x, A = 0, level = 0.95) {
+aug_t <- function(x, A = 0, level = 0.95, wt = 1) {
   n <- length(x)
   alpha <- 1 - level
   TOL <- sqrt(.Machine$double.eps)
-  if (n %in% augtbounds$n && any(abs(alpha - augtbounds$alpha) < TOL)) {
+  if (n %in% augtbounds$n && any(abs(alpha - augtbounds$alpha) < TOL) && wt == 1) {
     eta <- augtbounds$eta[augtbounds$n == n & abs(alpha - augtbounds$alpha) < TOL]
     nu <- augtbounds$nu[augtbounds$n == n & abs(alpha - augtbounds$alpha) < TOL]
   } else {
-    eout <- eta_alpha(alpha = alpha, n = n)
+    eout <- eta_alpha(alpha = alpha, n = n, wt = wt)
     eta <- eout[["eta"]]
     nu <- eout[["nu"]]
   }
 
-  mu_hat <- mean(c(x, A))
-  sig2_hat <- stats::var(c(x, A))
+  mu_hat <- mean_aug(x = x, A = A, wt = wt)
+  sig2_hat <- var_aug(x = x, A = A, wt = wt)
 
   c(
-    mu_hat - eta * sqrt(sig2_hat) / sqrt(n + 1),
-    mu_hat + eta * sqrt(sig2_hat) / sqrt(n + 1)
+    mu_hat - eta * sqrt(sig2_hat) / sqrt(n + wt),
+    mu_hat + eta * sqrt(sig2_hat) / sqrt(n + wt)
   )
 }
+
+#' Augmented mean
+#'
+#' If wt is an integer, this is mean(c(x, rep(A, wt))). But we generalize it to
+#' non-integer wt as well.
+#'
+#' @param x Data
+#' @param A Prior value
+#' @param wt Weight on prior value.
+#'
+#' @author David Gerard
+#'
+#' @export
+#'
+#' @return The augmented mean
+#'
+#' @examples
+#' mean_aug(c(1, 2, 3), 4, wt = 10)
+#'
+mean_aug <- function(x, A, wt = 1) {
+  n <- length(x)
+  n / (n + wt) * mean(x) + wt / (n + wt) * A
+}
+
+#' Augmented variance
+#'
+#' If wt is an integer, this is var(c(x, rep(A, wt))). But we generalize it to
+#' non-integer wt as well.
+#'
+#' @param x Data
+#' @param A Prior value
+#' @param wt Weight on prior value.
+#'
+#' @author David Gerard
+#'
+#' @export
+#'
+#' @return The augmented variance
+#'
+#' @examples
+#' var_aug(c(1, 2, 3), 4, wt = 10)
+#'
+var_aug <- function(x, A, wt = 1) {
+  n <- length(x)
+  (n - 1) / (n + wt - 1) * stats::var(x) + n * wt * (mean(x) - A)^2 / ((n + wt - 1) * (n + wt))
+}
+
+
--- tests/testthat/test-n2.R
+++ tests/testthat/test-n2.R
@@ -63,3 +63,88 @@ test_that("old and new augtbounds are same", {
   load("./augtbounts_old.RData")
   expect_equal(augtbounds$eta, augtbounds_old$eta, tolerance = 1e-3)
 })
+
+test_that("w_z hasn't changed with weight when wt = 1", {
+  w_z_old <- function(z, n, eta2, nu) {
+    ((n^2 - eta2) * z^2 - 2 * (n^(1.5) + sqrt(n) * eta2) * nu * z + (1 - eta2) * n * nu^2) / ((n + 1) * eta2)
+  }
+  expect_equal(
+    w_z(z = 1.3, n = 3, eta2 = 1.1, nu = 0.9, wt = 1),
+    w_z_old(z = 1.3, n = 3, eta2 = 1.1, nu = 0.9)
+  )
+
+  expect_equal(
+    w_z(z = 0.9, n = 10, eta2 = 0.1, nu = 1, wt = 1),
+    w_z_old(z = 0.9, n = 10, eta2 = 0.1, nu = 1)
+  )
+})
+
+test_that("mean_aug() and var_aug() work", {
+  set.seed(10)
+  x <- rnorm(4)
+  A <- 3
+  wt <- 2
+  expect_equal(
+    mean_aug(x = x, A = A, wt = 2),
+    mean(c(x, rep(A, wt)))
+  )
+  expect_equal(
+    var_aug(x = x, A = A, wt = 2),
+    stats::var(c(x, rep(A, wt)))
+  )
+})
+
+test_that("wt = 1 gives same augbounds as before", {
+  expect_equal(
+    eta_alpha(alpha = 0.05, n = 4, wt = 1)[["eta"]],
+    augtbounds$eta[augtbounds$alpha == 0.05 & augtbounds$n == 4]
+  )
+
+  expect_equal(
+    eta_alpha(alpha = 0.01, n = 20, wt = 1)[["eta"]],
+    augtbounds$eta[augtbounds$alpha == 0.01 & augtbounds$n == 20]
+  )
+
+  expect_equal(
+    eta_alpha(alpha = 0.1, n = 80, wt = 1)[["eta"]],
+    augtbounds$eta[augtbounds$alpha == 0.1 & augtbounds$n == 80]
+  )
+
+  expect_equal(
+    eta_alpha(alpha = 0.2, n = 3, wt = 1)[["eta"]],
+    augtbounds$eta[augtbounds$alpha == 0.2 & augtbounds$n == 3]
+  )
+})
+
+
+test_that("Coverage is correct for larger weight", {
+  set.seed(1)
+  n <- 2
+  wt <- 10
+  alpha <- 0.05
+  mu <- 3
+  A <- 1
+  nrep <- 1000000
+
+  eout <- eta_alpha(alpha = alpha, n = n, wt = wt)
+  eta <- eout[["eta"]]
+  nu <- eout[["nu"]]
+
+  sigma <- abs(mu - A) / nu
+
+  x <- matrix(stats::rnorm(n = n * nrep, mean = mu, sd = sigma), ncol = n)
+
+  mean_vec <- apply(X = x, MARGIN = 1, FUN = mean_aug, A = A, wt = wt)
+  var_vec <- apply(X = x, MARGIN = 1, FUN = var_aug, A = A, wt = wt)
+
+  lower <- mean_vec - eta * sqrt(var_vec) / sqrt(n + wt)
+  upper <- mean_vec + eta * sqrt(var_vec) / sqrt(n + wt)
+
+  nfail <- sum(mu < lower | mu > upper)
+
+  binom.test(x = nfail, n = nrep)
+})
+
+
+
+
